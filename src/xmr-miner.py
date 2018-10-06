109a45920e25faee7171e0f0ba62cf45c50b9fc2
Kevin Menegatti <kevin.menegatti@gmail.com>
2018-10-06 19:18:08 +0200

    v9 fork support added (October 2018 fork)

--- src/xmr-miner.py
+++ src/xmr-miner.py
@@ -17,7 +17,7 @@ def init():
     parser.add_argument('-o', '--url', help = 'mining pool - eg: stratum+tcp://mining.pool.com:3333 (required, if not file given)')
     parser.add_argument('-u', '--username', default = '', help = 'username or wallet address (required, if not file given)')
     parser.add_argument('-p', '--password', default = '', help = 'password (optional)')
-    parser.add_argument('-v', '--variant', default = '1', help = 'pow variant 0 (original) or 1 (April 2018 fork) (default = 1)')
+    parser.add_argument('-v', '--variant', default = '2', help = 'pow variant 0 (original) or 1 (April 2018) or 2 (October 2018) (default = 2)')
     parser.add_argument('-t', '--threads', default ='1', help = 'mining threads (default = 1)')
     parser.add_argument('-d', '--debug', action='store_true')
     options = parser.parse_args(sys.argv[1:])
@@ -43,10 +43,10 @@ def init():
             options.url = 'stratum+tcp://' + options.url
         try:
             int(options.variant)
-            if int(options.variant) > 1:
-                options.variant = 1
+            if int(options.variant) > 2:
+                options.variant = 2
         except:
-            options.variant = 1
+            options.variant = 2
         try:
             int(options.threads)
         except:
@@ -61,7 +61,7 @@ def init():
         print('  1  URL         mining pool - eg: stratum+tcp://mining.pool.com:3333 (required)')
         print('  2  USERNAME    username or wallet address (required)')
         print('  3  PASSWORD    password (optional)')
-        print('  4  VARIANT     pow variant 0 (original) or 1 (April 2018 fork) (default = 1)')
+        print('  4  VARIANT     pow variant 0 (original) or 1 (April 2018) or 2 (October 2018) (default = 2)')
         print('  5  THREADS     mining threads (default = 1)')
         print('\n')
         input('Press ENTER to quit...')
